76cfffed0936be39297a2ac17e80847af9c65e96
hopedavids <hopeolawole@gmail.com>
2023-10-26 23:40:46 +0000

    replaced the signal function with provider

--- ripple/utils/ethersProvider.js
+++ ripple/utils/ethersProvider.js
@@ -1,8 +1,8 @@
-// import { ethers } from 'ethers';
+import { ethers } from 'ethers';
 require('dotenv').config();
-const ethers = require("ethers");
+// const ethers = require("ethers");
 
-const INFURA_API_KEY = process.env.INFURA_API_KEY;
+// const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
 // The actual hardhat network url goes here
 const networkURL = 'https://sepolia.infura.io/v3/2db2b7fe04034b08880ff7a62d624b17';
@@ -10,9 +10,13 @@ const ethersprovider = new ethers.JsonRpcProvider(networkURL);
 
 const privateKey = '95d0c9d019793cd2bda39f93064aa60bbf64ca9abba40c22566effc5524b3cb6';
 
-// Initialize the provider with the account
-const wallet = new ethers.Wallet(privateKey, ethersprovider);
-const provider = wallet.connect(ethersprovider);
+// Initialize the wallet
+const wallet = new ethers.Wallet(privateKey);
 
+// Connect the wallet to the provider
+const provider = new ethers.JsonRpcProvider(networkURL);
+const connectedWallet = wallet.connect(provider);
 
-export default {provider,ethersprovider};
+export default connectedWallet;
+
+// export default provider;
