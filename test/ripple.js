36c59bdf4fc6c33a87c8b1fe3a1a896b07c261cb
rawk akani <lionelmbankeu@gmail.com>
2023-10-26 00:22:05 +0000

    added tests

--- test/ripple.js
+++ test/ripple.js
@@ -2,61 +2,56 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Crowdfunding", function () {
-    let Crowdfunding, crowdfunding, owner, addr1, addr2;
+  let Crowdfunding, crowdfunding, owner, addr1, addr2;
 
-    beforeEach(async () => {
-        Crowdfunding = await ethers.getContractFactory("Crowdfunding");
-        [owner, addr1, addr2, _] = await ethers.getSigners();
-        crowdfunding = await Crowdfunding.deploy();
+  beforeEach(async () => {
+    Crowdfunding = await ethers.getContractFactory("Crowdfunding");
+    [owner, addr1, addr2, _] = await ethers.getSigners();
+    crowdfunding = await Crowdfunding.deploy();
+  });
+
+  describe("createCampaign", function () {
+    it("Should create a new campaign", async function () {
+      await crowdfunding.createCampaign("Test Business", 100, 604800, 604800, 10);
+      const campaign = await crowdfunding.getCampaignInfo(0);
+      expect(campaign.businessName).to.equal("Test Business");
     });
+  });
 
-    describe("createCampaign", function () {
-        it("Should create a new campaign", async function () {
-            await crowdfunding.createCampaign("Test Business", 100, 604800, 604800, 10);
-            const campaign = await crowdfunding.getCampaignInfo(0);
-            expect(campaign.businessName).to.equal("Test Business");
-        });
-    });
+  describe("deposit", function () {
+      it("Should allow an investor to deposit funds", async function () {
+          await crowdfunding.createCampaign("Test Business", 100, 604800, 604800, 10);
+          await crowdfunding.deposit(0, 1, 150);  // Investor ID 1, depositing 150
+          const campaign = await crowdfunding.getCampaignInfo(0);
+          expect(parseInt(campaign.fundsRaised.toString())).to.equal(150);
+      });
 
-    describe("registerInvestor", function () {
-        it("Should register a new investor", async function () {
-            await crowdfunding.registerInvestor(1, { from: addr1.address });
-            const investorAddress = await crowdfunding.investors(1);
-            expect(investorAddress).to.equal(addr1.address);
-        });
-    });
+      it("Should revert if the amount is less than 101", async function () {
+          await crowdfunding.createCampaign("Test Business", 100, 604800, 604800, 10);
+          await expect(crowdfunding.deposit(0, 1, 100)).to.be.revertedWith("Must deposit a amount greataer then 100");
+      });
+  });
 
-    describe("deposit", function () {
-        it("Should allow an investor to deposit funds", async function () {
-            await crowdfunding.createCampaign("Test Business", 100, 604800, 604800, 10);
-            await crowdfunding.registerInvestor(1, { from: addr1.address });
-            await crowdfunding.deposit(0, 1, 50, { from: addr1.address });
-            const campaign = await crowdfunding.getCampaignInfo(0);
-            expect(campaign.fundsRaised.toNumber()).to.equal(50);
-        });
-    });
+  describe("withdraw", function () {
+      it("Should allow an investor to withdraw funds after the waiting period", async function () {
+          await crowdfunding.createCampaign("Test Business", 100, 1, 1, 10);  // Very short durations for testing
+          await crowdfunding.deposit(0, 1, 150);
 
-    describe("withdraw", function () {
-        it("Should allow an investor to withdraw funds after the waiting period", async function () {
-            await crowdfunding.createCampaign("Test Business", 100, 1, 1, 10);  // Very short durations for testing
-            await crowdfunding.registerInvestor(1, { from: addr1.address });
-            await crowdfunding.deposit(0, 1, 50, { from: addr1.address });
+          // Simulate some delay (e.g., by mining some empty blocks)
+          for (let i = 0; i < 10; i++) {
+              await ethers.provider.send("evm_mine");
+          }
 
-            // Simulate some delay (e.g., by mining some empty blocks)
-            for (let i = 0; i < 10; i++) {
-                await ethers.provider.send("evm_mine");
-            }
+          await crowdfunding.withdraw(0, 1);
+          // Assuming there is a function to check an investor's withdrawn funds
+      });
+  });
 
-            await crowdfunding.withdraw(0, 1, { from: addr1.address });
-            // Add your checks here, e.g., check balance of addr1, check campaign funds, etc.
-        });
-    });
-
-    describe("getCampaignInfo", function () {
-        it("Should return the correct campaign info", async function () {
-            await crowdfunding.createCampaign("Test Business", 100, 604800, 604800, 10);
-            const campaign = await crowdfunding.getCampaignInfo(0);
-            expect(campaign.businessName).to.equal("Test Business");
-        });
-    });
+  describe("getCampaignInfo", function () {
+      it("Should return the correct campaign info", async function () {
+          await crowdfunding.createCampaign("Test Business", 100, 604800, 604800, 10);
+          const campaign = await crowdfunding.getCampaignInfo(0);
+          expect(campaign.businessName).to.equal("Test Business");
+      });
+  });
 });
